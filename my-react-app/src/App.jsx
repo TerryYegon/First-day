02ac43dfa55609c47c8998cb6d662cc105350be9
TerryYegon <yegonterry618@gmail.com>
2026-06-09 04:05:37 -0700

    changes by Tee

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -16,11 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-<<<<<<< HEAD
           <h1>Get started by Terry Yegon</h1>
-=======
-          <h1>Get started By Rhys Murage</h1>
->>>>>>> 281d49e (Changes made by Rhys)
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
